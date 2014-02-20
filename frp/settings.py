4204a3cc4c7c8d271db6138f7d878a3acbe695de
Asmund <asmundliseth@gmail.com>
2014-02-20 21:24:54 +0100

    Converted wishlist to south

--- frp/settings.py
+++ frp/settings.py
@@ -125,7 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gunicorn',
-    # 'south',
+    'south',
     'wishlist',
     'budget',
     'filters',
