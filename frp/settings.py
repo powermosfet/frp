3319d2456acfab5511cb43a061c8c7fe4a4d1a15
Asmund <asmundliseth@gmail.com>
2014-03-03 22:54:37 +0100

    Updated db settings

--- frp/settings.py
+++ frp/settings.py
@@ -21,15 +21,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'database.db',                  # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+    'default': dj_database_url.config()
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
