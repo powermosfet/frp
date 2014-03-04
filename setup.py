58c59a4dec6ef5595b5a85d71ec20f61e6b50efc
Asmund <asmundliseth@gmail.com>
2014-03-04 19:23:34 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+setup(
+        name='mymodule',
+        version='0.1.0',
+        install_requires=[],  # Don't put anything here, just use requirements.txt
+        packages=['mymodule'],
+        package_dir={'mymodule': 'src/mymodule'},
+        )
