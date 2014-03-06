6041eda76e6d43f39406ee823ffe085a0adcbb46
Asmund Berge Liseth <asmundliseth@gmail.com>
2014-03-06 20:51:09 +0100

    Adjusted settings.py and wsgi.py according to
    https://devcenter.heroku.com/articles/getting-started-with-django

--- frp/settings.py
+++ frp/settings.py
@@ -24,9 +24,12 @@ DATABASES = {
     'default': dj_database_url.config()
 }
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
