659cb5afef9fa37e2d0920edd52d1e3138fa7164
Asmund <asmundliseth@gmail.com>
2014-03-21 18:37:26 +0100

    Added syncdb and migrate to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,7 @@
 virtualenv -p `which python2` --no-site-packages .
 source bin/activate
 pip install -r requirements.txt
-export DATABASE_URL="sqlite://database.sqlite"
-foreman start -f Procfile_dev
+export DATABASE_URL="sqlite:///database.sqlite"
+python2 manage.py syncdb
+python2 manage.py migrate
+python2 manage.py runserver
